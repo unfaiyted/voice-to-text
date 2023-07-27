451c848567e534a0b0e1a38ad68076cd8356bd74
Dane Miller <unfaiyted@gmail.com>
2023-07-27 12:37:10 -0500

    feature: able to work with media recorder function but errors with cutting audio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,98 @@
 <script>
-	import Counter from './Counter.svelte';
+	import { onDestroy } from 'svelte';
 
-		let transcript = '';
-		let mediaRecorder = null;
-		let audioChunks = [];
+	let mediaRecorder;
+	let audioQueue = [];
+	let sendIntervalId = null;
+	let isRecording = false;
+	let requestInProgress = false;
+	const QUEUE_MAX_TIME = 20000;  // 30 seconds
+	let transcript = '';
 
-		const startRecording = async () => {
-		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-		mediaRecorder = new MediaRecorder(stream);
-		mediaRecorder.start();
+	async function startRecording() {
+		if (!isRecording) {
+			console.log("Start recording...");
+			const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+			mediaRecorder = new MediaRecorder(stream);
+			mediaRecorder.addEventListener("dataavailable", event => {
+				audioQueue.push({data: event.data, timestamp: Date.now()});
 
-		mediaRecorder.addEventListener("dataavailable", event => {
-			audioChunks.push(event.data);
-		});
+				// Remove chunks from the queue that are older than QUEUE_MAX_TIME
+				// Cut oldest audio data from queue if it exceeds QUEUE_MAX_TIME
 
-		mediaRecorder.addEventListener("stop", async () => {
-			const audioBlob = new Blob(audioChunks);
-			audioChunks = []; // clear previous audio data
+				 console.log('audioQueue', audioQueue);
 
-			// Construct form data
-			let formData = new FormData();
-			formData.append("audio_file", audioBlob);
+				while ((audioQueue[audioQueue.length - 1].timestamp - audioQueue[0].timestamp) > QUEUE_MAX_TIME) {
+					console.log("Trimming audio queue...");
 
-			// Make the HTTP POST request to the ASR endpoint
-			const response = await fetch('http://192.168.0.120:9000/asr', {
-			method: 'POST',
-			body: formData,
-	});
+					console.log('trimmed-audioQueue', audioQueue);
+					// Instead of removing a fixed number of chunks, we remove chunks until we're below QUEUE_MAX_TIME again
+					// This ensures we don't accidentally cut an audio chunk in half
+					audioQueue.shift();
+					if (audioQueue.length === 0) {
+						break;
+					}
+				}
 
-		if (response.ok) {
-			console.log('Success!', response)
-		const data = await response.text();
-		transcript = data; // this depends on the structure of the API response
-	} else {
-		console.error('Error:', response.status, response.statusText);
+			});
+
+			mediaRecorder.start(1000);
+
+			sendIntervalId = setInterval(async () => {
+				if(requestInProgress) {
+					return;
+				}
+
+				// Only send data if there's at least 2 seconds of audio (2000ms)
+				if ((audioQueue[audioQueue.length - 1].timestamp - audioQueue[0].timestamp) >= 1000) {
+					console.log("Sending data to server...");
+					requestInProgress = true;
+
+					// Construct form data
+					let formData = new FormData();
+					let audioBlob = new Blob(audioQueue.map(chunk => chunk.data));
+
+					// Log the size of the blob
+					console.log(`Sending Blob with size: ${audioBlob.size} bytes`);
+
+					formData.append("audio_file", audioBlob);
+
+					// Make the HTTP POST request to the ASR endpoint
+					const response = await fetch('http://192.168.0.120:9000/asr', {
+						method: 'POST',
+						body: formData,
+					});
+
+					if (response.ok) {
+						const data = await response.text();
+						console.log("Received response from server:", data);
+						transcript = data; // replace the old transcript with the new one
+					} else {
+						console.error('Error:', response.status, response.statusText);
+					}
+
+					requestInProgress = false;
+				}
+			}, 4000);
+
+			isRecording = true;
+		}
 	}
+
+	function stopRecording() {
+		if (isRecording) {
+			console.log("Stop recording...");
+			// Stop the mediaRecorder and the sending interval
+			mediaRecorder.stop();
+			clearInterval(sendIntervalId);
+			sendIntervalId = null;
+			isRecording = false;
+		}
+	}
+
+	onDestroy(() => {
+		stopRecording();
 	});
-	};
-
-		const stopRecording = () => {
-		mediaRecorder.stop();
-	};
-
 </script>
 
 <svelte:head>
@@ -61,7 +112,6 @@
 	</div>
 
 
-	<Counter />
 </section>
 
 <style>
